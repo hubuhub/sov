a1ae93a336d548ba91ee366c95ffb2163c7226e1
hubuhub <39082451+hubuhub@users.noreply.github.com>
2019-09-30 17:02:02 +0300

    Update lunrsearchengine.js

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -21,10 +21,16 @@ var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or p
     "body": "{{ page.date | date: "%Y/%m/%d" }} - {{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
     }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}];
 
+var lunr = require("lunr")
+require("{{ site.url }}{{site.baseurl}}assets/js/lunr.stemmer.support")(lunr)
+require("{{ site.url }}{{site.baseurl}}assets/js/lunr.ru")(lunr)
+
 var idx = lunr(function () {
-    this.ref('id')
-    this.field('title')
-    this.field('body')
+  this.use(lunr.ru)
+  this.ref('id')
+  this.field('text')
+  this.field('body')
+})
 
     documents.forEach(function (doc) {
         this.add(doc)
